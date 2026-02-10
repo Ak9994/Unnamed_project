85bb13e3dd620b532146aceaef8e874c61b41c1c
Ak9994 <163250688+Ak9994@users.noreply.github.com>
2026-02-10 15:47:11 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
-#include<iostream>
+#include<include/Escape.h>
+#include<include/GameConfig.h>
 
-main() {
-	std::cout << "Hello World" << "\n";
+int main() {
+	GameConfig PlayerCfg;
+	//Set Age
+	PlayerCfg.start_age = 20;
+	PlayerCfg.max_age = 100;
+	//Set Game Limits
+	PlayerCfg.GameEnd_TimeLimit = 5;
+	PlayerCfg.Level_TimeLimit = 1;
+	//Set Exit
+	PlayerCfg.ExitSpawn_Count = 20;
+	//Testing
+	PlayerCfg.Idleplay = true;
+	//Exit Selection Prob
+	PlayerCfg.CorrectSelectionProb = 0.05;
+
+	Escape Escape(PlayerCfg);
+	Escape.GameLoop();
+
+	return 0;
 }
+
